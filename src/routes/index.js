79ff8492a19ebb83027fec771db274bcdeae29f2
Artur Felinto <arturfelinto07@gmail.com>
2022-09-15 20:32:10 -0300

    Modificação nas rotas para autenticação

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const AtendimentosController = require("../controllers/atendimento.controller");
 const painel = require('../controllers/painel.controller');
+const atendimentoValidacao = require("../validation/atendimentos/atendimentoValidacao");
 
 routes.get('/atendimentos', AtendimentosController.listAttendances);
 routes.get('/atendimentos/:id', AtendimentosController.listOfAttendance);
 routes.post('/atendimentos', AtendimentosController.registerAttendance);
+routes.post('/atendimentos', atendimentoValidacao, AtendimentosController.registerAttendance);
 
 routes.get('/painel/atendimentos', painel.numberAtendimentos);
 routes.get('/painel/psicologos/media', painel.atendimentoPsicologo);
